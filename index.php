8c4ac93e7ba84c488b585bc2f5c16ff96a615a30
smky <34562776+smky@users.noreply.github.com>
2018-10-12 13:47:31 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,26 @@
 <?
+function getRealIpAddr()
+{
+    if (!empty($_SERVER['HTTP_CLIENT_IP']))   //check ip from share internet
+    {
+      $ip=$_SERVER['HTTP_CLIENT_IP'];
+    }
+    elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR']))   //to check ip is pass from proxy
+    {
+      $ip=$_SERVER['HTTP_X_FORWARDED_FOR'];
+    }
+    else
+    {
+      $ip=$_SERVER['REMOTE_ADDR'];
+    }
+    return $ip;
+}
+$ClientIP = getRealIpAddr();
 
 echo "Server IP Address:". $_SERVER['SERVER_ADDR']."		";
-echo "Your IP Address:". $_SERVER['REMOTE_ADDR'].PHP_EOL;
+echo "Your IP Address:". $ClientIP.PHP_EOL;
 
-
+foreach (getallheaders() as $name => $value) {
+    echo "$name: $value<br>\n";
+}
 ?>
